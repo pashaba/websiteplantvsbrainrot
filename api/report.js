35d028595fda1c40b2d5d0750d2b0bb51afc95f9
PashaDev <kimtha293@gmail.com>
2025-10-14 17:11:42 +0700

    Update report.js

--- api/report.js
+++ api/report.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGO_URI; // kita ambil dari environment variable
+const uri = process.env.MONGO_URL; // kita ambil dari environment variable
 let client;
 
 async function connectDB() {
